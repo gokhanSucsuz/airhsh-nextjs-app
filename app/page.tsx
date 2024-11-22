bfe07cd258662d08a8232bd77896281b1729e83a
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-11-22 11:11:45 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
 import { Suspense } from "react";
 
-const HomePage = async ({
-	searchParams
-}: {
-	searchParams: { category?: string; search?: string };
-}) => {
-	const { category, search } = searchParams;
-	return (
+const HomePage = async (
+    props: {
+        searchParams: Promise<{ category?: string; search?: string }>;
+    }
+) => {
+    const searchParams = await props.searchParams;
+    const { category, search } = searchParams;
+    return (
 		<section>
 			<CategoriesList searchParams={searchParams} />
 			<Suspense fallback={<LoadingCards />}>
